babec9fc1ac257cf0f5a134634b997d0b31ac0ec
HannahChege <chegehannah45@gmail.com>
2018-09-14 15:28:44 +0300

    Added content in the init py

--- app/main/views.py
+++ app/main/views.py
@@ -4,25 +4,12 @@ from . import main
 from flask_login import login_required
 
 
-# Views
-# @main.route('/upvote/new/<int:pitch_id>', methods = ['GET','POST'])
-# @login_required
-# def new_upvote(pitch_id):
-#     '''
-#     View the root page function
-#     '''
 
 @main.route('/')
 def index():
     '''
     View the root page function
     '''
-    title = "Pitches"
+    title = "Blog"
     return render_template('index.html', title = title)
 
-@main.route('/new/pitch', methods = ['GET','POST'])
-@login_required
-def new_pitch(pitch_id):
-    '''
-    View the root page function
-    ''

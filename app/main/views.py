e30691321c22e4260fb8ee7e5ae81780c75861cc
HannahChege <chegehannah45@gmail.com>
2018-09-14 23:43:08 +0300

    Added content to the views .py

--- app/main/views.py
+++ app/main/views.py
@@ -2,24 +2,23 @@ from flask import Flask
 from flask import render_template,redirect,url_for
 from . import main
 from flask_login import login_required
-from .forms import PitchForm
+from .forms import BlogForm, CommentForm ,EmailForm
 from .. models import User
 from ..import db
 
-@main.route('/')
-def index():
-    pitches = Pitch.query.all()
-    '''
-    View the root page function
-    '''
-    title = "Pitches"
-    return render_template('index.html', title = title,pitches=pitches)
+# @main.route('/')
+# def index():
+#     form = EmailForm()
+#     '''
+#     View the root page function
+#     '''
+#     title = "Pitches"
+#     return render_template('index.html', title = title,pitches=pitches)
 
-@main.route('/new/pitch', methods = ['GET','POST'])
-@login_required
-def new_pitch():
-    formpitch = PitchForm()
-    if formpitch.validate_on_submit():
+@main.route('/', methods = ['GET','POST'])
+def index():
+    formemail = EmailForm()
+    if formemail.validate_on_submit():
         pitch = Pitch(category = formpitch.category.data, content = formpitch.pitch.data)
         db.session.add(pitch)
         db.session.commit()
@@ -27,13 +26,13 @@ def new_pitch():
      
     return render_template('pitch.html',formpitch = formpitch)   
 
-@main.route('/comment/new/<int:pitch_id>', methods = ['GET','POST'])
-@login_required
-def new_comment(pitch_id):  
-    '''
-    View the root page function
-    '''
-    pass
+# @main.route('/comment/new/<int:pitch_id>', methods = ['GET','POST'])
+# @login_required
+# def new_comment(pitch_id):  
+#     '''
+#     View the root page function
+#     '''
+#     pass
 
 
 

42838ac31a6d89aebfe36c2f556380ea91a06cf0
HannahChege <chegehannah45@gmail.com>
2018-09-16 16:34:08 +0300

    Addede the registration route in the views.py in the auth

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,3 +4,14 @@ from . import auth
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        admin = Admin(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(admin)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)

5215d08a763d8ad2b7a600d0b5fc90eb056afff2
HannahChege <chegehannah45@gmail.com>
2018-09-16 16:26:34 +0300

    Added the login manager in the models.py

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class Admin(UserMixin,db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
     pass_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
 
     @property
     def password(self):

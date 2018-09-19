a0aa5c1b51eacdd7de5198e78f2d42bacfb967dd
HannahChege <chegehannah45@gmail.com>
2018-09-19 09:25:25 +0300

    Updated database

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
-    # SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://hannah:hannah@localhost/perblog'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://hannah:hannah@localhost/perblog'
 
     DEBUG = True
 
